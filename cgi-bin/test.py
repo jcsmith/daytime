85d0504b35539393ab18cc596e2921d9dd18ad23
Josh Smith <jsmith@mail.wvnet.edu>
2016-05-29 09:45:37 -0400

    Start returning a json object in addition to the html from test.py.

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -9,10 +9,14 @@ fs = cgi.FieldStorage()
 
 location = json.loads(fs.getvalue('loc'))
 myObserver = ephem.Observer()
-myObserver.date =  '2016/05/25 17:00'
+myObserver.date =  '2016/05/29 17:00'
 myObserver.lat = str(location['latitude'])
 myObserver.lon = str(location['longitude'])
 
+dictMyObserver = {}
+
+dictMyObserver['sunrise'] = myObserver.previous_rising(ephem.Sun()).datetime().isoformat()
+dictMyObserver['sunset'] = myObserver.next_setting(ephem.Sun()).datetime().isoformat()
 
 print("Content-type:text/html\r\n\r\n")
 print ("Sunrise: ")
@@ -21,3 +25,4 @@ print (" UTC </br>")
 print ("Sunset: ")
 print (myObserver.next_setting(ephem.Sun()))
 print (" UTC </br>")
+print (json.dumps(dictMyObserver))
