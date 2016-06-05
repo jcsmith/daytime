da8622f1c91cbc31d67c3caf4345236994398d72
Josh Smith <jsmith@mail.wvnet.edu>
2016-06-05 11:01:37 -0400

    Add comments to describe the function of the python script.

--- cgi-bin/daytime.py
+++ cgi-bin/daytime.py
@@ -8,15 +8,20 @@ import json
 fs = cgi.FieldStorage()
 
 location = json.loads(fs.getvalue('loc'))
+
+#Convert timestamp to python datetime object.
 location['timeStampAsDate'] = datetime.datetime.strptime(location['timeStampAsDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+
+#Create pyephem observer using the timestamp and coordinates provided.
 myObserver = ephem.Observer()
 myObserver.date = str(location['timeStampAsDate'])
 myObserver.lat = str(location['latitude'])
 myObserver.lon = str(location['longitude'])
 
+#Create an empty dictionary to hold results of sunrise/sunset calculations for json serialization.
 dictMyObserver = {}
 
-#Since we are primarly integrating with javascript on the front end and it seems to assume
+#Since we are primarly integrating with javascript on the frontend and it seems to assume
 #a datetime string with no time zone specified is local time and _NOT_ UTC time we must
 #append a Z to the end of the datetime string.  This seems like a hack but I have not been able
 #to find a better solution anywhere online.
@@ -24,5 +29,6 @@ dictMyObserver = {}
 dictMyObserver['sunrise'] = myObserver.previous_rising(ephem.Sun()).datetime().isoformat() + 'Z'
 dictMyObserver['sunset'] = myObserver.next_setting(ephem.Sun()).datetime().isoformat() + 'Z'
 
+#"Return" a JSON object to the webserver.
 print("Content-type:application/json\r\n\r\n")
 print (json.dumps(dictMyObserver))
