6bb3cd32b507a12a35e3f29b9f118aa12698d5c9
Josh Smith <jsmith@mail.wvnet.edu>
2016-06-05 15:08:07 -0400

    Return HTTP 400 Bad Request errors when we fail to load submitted data.

--- cgi-bin/daytime.py
+++ cgi-bin/daytime.py
@@ -4,22 +4,63 @@ import cgi
 import datetime
 import ephem
 import json
+import sys
 
-fs = cgi.FieldStorage()
+##################################################
+#Since we are actively developing the script lets include cgitb
+#and have it log to a fike.  This will be removed once we are at
+#a more steady state.
+##################################################
+import cgitb
+cgitb.enable(display=0, logdir="/var/log/cgitb.log")
 
-location = json.loads(fs.getvalue('loc'))
+#function to return HTTP 400 Bad Request Error
+def returnHTTP400 ():
+    print ('Status: 400 Bad Request')
+    print ()
 
-#Convert timestamp to python datetime object.
-location['timeStampAsDate'] = datetime.datetime.strptime(location['timeStampAsDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
 
-#Create pyephem observer using the timestamp and coordinates provided.
-myObserver = ephem.Observer()
-myObserver.date = str(location['timeStampAsDate'])
-myObserver.lat = str(location['latitude'])
-myObserver.lon = str(location['longitude'])
 
 #Create an empty dictionary to hold results of sunrise/sunset calculations for json serialization.
 dictMyObserver = {}
+
+fs = cgi.FieldStorage()
+
+location = fs.getfirst('loc')
+try:
+    location = json.loads(location)
+except:
+    returnHTTP400()
+    sys.exit()
+
+
+
+offset = fs.getfirst("offset")
+try:
+    offset = json.loads(offset)
+except:
+    dictMyObserver['offset'] = "0"
+
+
+
+
+#Convert timestamp to python datetime object.
+try:
+    location['timeStampAsDate'] = datetime.datetime.strptime(location['timeStampAsDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+except:
+    returnHTTP400()
+    sys.exit()
+
+#Create pyephem observer using the timestamp and coordinates provided.
+try:
+    myObserver = ephem.Observer()
+    myObserver.date = str(location['timeStampAsDate'])
+    myObserver.lat = str(location['latitude'])
+    myObserver.lon = str(location['longitude'])
+except:
+    returnHTTP400()
+    sys.exit()
+
 
 #Since we are primarly integrating with javascript on the frontend and it seems to assume
 #a datetime string with no time zone specified is local time and _NOT_ UTC time we must
